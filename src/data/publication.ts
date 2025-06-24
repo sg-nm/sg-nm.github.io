a28ad3d8e42a9e8ea683d73dca239653f6010d9a
Masanori Suganuma <ykhmprsi1029@gmail.com>
2025-06-24 18:25:10 +0900

    Update publication.ts

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -26,7 +26,7 @@ export const publicationData: Publication[] = [
     //  "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     //award: "🏆 Best Paper Award",
     // if you have an image in public/images, you can use it like this:
-    //imageUrl: "/images/DHPR.png"
+    imageUrl: "/images/OSOD3.png"
   },
   {
     year: "2025",
