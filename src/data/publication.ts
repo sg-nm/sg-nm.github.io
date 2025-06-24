e8e3477c12e862cb4b954cf776e0f859f639f276
Masanori Suganuma <ykhmprsi1029@gmail.com>
2025-06-24 18:23:14 +0900

    Update publication.ts

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -15,6 +15,21 @@ export const publicationData: Publication[] = [
   // If you don't want to show publications, just make the array empty.
   {
     year: "2025",
+    conference: "International Journal of Computer Vision",
+    title: "Rethinking Open-Set Object Detection: Issues, A New Formulation, and Taxonomy",
+    authors: "Yusuke Hosoya, Masanori Suganuma, Takayuki Okatani",
+    paperUrl: "https://link.springer.com/article/10.1007/s11263-025-02479-3",
+    //codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
+    //bibtex: "https://arxiv.org/abs/2409.15476.bib",
+    //tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
+    //imageUrl:
+    //  "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //award: "🏆 Best Paper Award",
+    // if you have an image in public/images, you can use it like this:
+    //imageUrl: "/images/DHPR.png"
+  },
+  {
+    year: "2025",
     conference: "ICML",
     title: "Self-Supervised Learning of Intertwined Content and Positional Features for Object Detection",
     authors: "Kang-Jun Liu, Masanori Suganuma, Takayuki Okatani",
